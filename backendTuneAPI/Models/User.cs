eb5b113b4d5835360d93b99d85c3a732b9311ff0
Justin <kayuuha@gmail.com>
2024-10-22 16:21:31 -0700

    Added CRUD operations. Created new UsersService singleton to ensure only one mongoDB Atlas connection. Adjusted secrets.json to be more malleable for databaseName and collectionsName

---
+++ backendTuneAPI/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MoodzApi.Models;
+public class User
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+    public string Email { get; set; } = null!;
+    public string Name { get; set; } = null!;
+}
---
+++ backendTuneAPI/Services/UsersService.cs
@@ -0,0 +1,34 @@
+using MoodzApi.Models;
+using MongoDB.Driver;
+using Microsoft.Extensions.Options;
+
+namespace MoodzApi.Services;
+
+public class UsersService
+{
+    private readonly IMongoCollection<User> _usersCollection;
+    public UsersService(
+        IOptions<UserDatabaseSettings> userDatabaseSettings)
+    {
+        var mongoClient = new MongoClient(userDatabaseSettings.Value.ConnectionString);
+
+        var mongoDatabase = mongoClient.GetDatabase(userDatabaseSettings.Value.DatabaseName);
+
+        _usersCollection = mongoDatabase.GetCollection<User>(userDatabaseSettings.Value.UsersCollectionName);
+    }
+
+    public async Task<List<User>> GetAsync() =>
+        await _usersCollection.Find(_ => true).ToListAsync();
+
+    public async Task<User?> GetAsync(string id) =>
+        await _usersCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+    public async Task CreateAsync(User newBook) =>
+        await _usersCollection.InsertOneAsync(newBook);
+
+    public async Task UpdateAsync(string id, User updatedBook) =>
+        await _usersCollection.ReplaceOneAsync(x => x.Id == id, updatedBook);
+
+    public async Task RemoveAsync(string id) =>
+        await _usersCollection.DeleteOneAsync(x => x.Id == id);
+}
