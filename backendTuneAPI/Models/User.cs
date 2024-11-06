b519ff8f0f33aff6e219deb243de8fc635c56ba0
Scott Lee <scottlee3447@gmail.com>
2024-11-06 14:23:06 -0800

    Removed all previous code related to storing/getting a user's authorization code upon login

--- backendTuneAPI/Models/User.cs
+++ backendTuneAPI/Models/User.cs
@@ -10,7 +10,6 @@ public class User
     public string Email { get; set; } = null!;
     public string Name { get; set; } = null!;
     public Entry[] Entries { get; set; } = null!;
-    public string SpotifyAuthenticationCode { get; set; } = null!;
     public SpotifyUserAccessToken SpotifyUserAccessToken { get; set; } = null!;
     public string SpotifyId { get; set; } = null!;
 }
