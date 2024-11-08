316458361016784e809f3d8fe59029b3f36c0304
Scott Lee <scottlee3447@gmail.com>
2024-11-08 01:20:54 -0800

    Fixed User Entries errors

--- backendTuneAPI/Models/UserState.cs
+++ backendTuneAPI/Models/UserState.cs
@@ -4,6 +4,6 @@ public class UserState
     public string Id { get; set; } = null!;
     public string Name { get; set; } = null!;
     public string ProfilePicUrl { get; set; } = null!;
-    public Entry[] Entries { get; set; } = null!;
+    public List<Entry> Entries { get; set; } = new List<Entry>()!;
     public string JwtToken { get; set; } = null!;
 }
