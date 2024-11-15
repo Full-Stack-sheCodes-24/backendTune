1c726f68d59f35b3dcb56285896e1def675baeb2
yuunster <kayuuha@gmail.com>
2024-11-15 02:18:29 -0800

    Added endpoint to save settings / color themes

--- backendTuneAPI/Models/Entry.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MoodzApi.Models;
-public class Entry
-{
-    public string Text { get; set; } = null!;
-    public int Likes { get; set; } = default!;
-    public Track Track { get; set; } = null!;
-    public DateTime? Date { get; set; } = null!;
-}
--- backendTuneAPI/Models/User.cs
+++ backendTuneAPI/Models/User.cs
@@ -17,4 +17,25 @@ public class User
     public string RefreshToken { get; set; } = null!;
     public DateTime? RefreshTokenExpiresAt { get; set; }
     public string SpotifyId { get; set; } = null!;
+    public Settings Settings { get; set; } = new Settings();
+}
+
+public class ProfileInfo
+{
+    public string? ProfilePicUrl { get; set; }
+    public string? BioText { get; set; }
+    public DateTime? Birthday { get; set; }
+}
+
+public class Entry
+{
+    public string Text { get; set; } = null!;
+    public int Likes { get; set; } = default!;
+    public Track Track { get; set; } = null!;
+    public DateTime? Date { get; set; } = null!;
+}
+
+public class Settings
+{
+    public string? Theme { get; set; }
 }
