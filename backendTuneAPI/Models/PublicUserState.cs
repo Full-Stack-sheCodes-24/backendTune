05b3f65521ebd9b30ea7f59cbf4913c9918babe7
Justin <kayuuha@gmail.com>
2024-11-17 23:30:06 -0800

    Do not send out private user information (like biotext, birthday, and entries) if the user has their privacy setting enabled

--- backendTuneAPI/Models/PublicUserState.cs
+++ backendTuneAPI/Models/PublicUserState.cs
@@ -1,10 +1,10 @@
 ﻿namespace MoodzApi.Models;
-public class PublicUserState
+public class PublicUserState : OtherUserState
 {
-    public string Id { get; set; } = null!;
     public string Name { get; set; } = null!;
     public string ProfilePicUrl { get; set; } = null!;
     public string BioText { get; set; } = null!;
     public DateTime? Birthday { get; set; }
     public List<Entry> Entries { get; set; } = new List<Entry>()!;
+    public bool isPrivate = false;
 }
